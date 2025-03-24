d2203973081809dfb5d0aa3d597dbd12f8d19e6e
Romain Gremaud <rgremau@gmail.com>
2025-03-24 16:33:48 -0400

    Add operate function
    Working on operate function.  Addition component works.
    All other math functions seem to call addition instead of proper
    operator.

--- javascript.js
+++ javascript.js
@@ -1,31 +1,45 @@
 // Math functions
 let sum = 0;
 
-function addition(a,b) {
+function add(a,b) {
     sum = a+b;
-    console.log(sum);
 }
 
-function subtraction(a,b) {
+function subtract(a,b) {
     sum = a-b;
-    console.log(sum);
-
 }
 
-function multiplication(a,b) {
+function multiply(a,b) {
     sum = a*b;
-    console.log(sum);
 }
 
-function division(a,b) {
+function divide(a,b) {
     sum = a / b;
-    console.log(sum);
-
 }
 
 function clearTotal() {
     sum = 0;
 }
+
+function operate(a,b,mathFunction) {
+    if (mathFunction = "add") {
+        add(a,b);
+        console.log("Addition is " + sum);
+    } else if (mathFunction = "subtract") {
+        subtract(a,b);
+        console.log("Subtraction is "+ sum);
+    } else if (mathFunction = "multiply") {
+        multiply(a,b);
+        console.log("Mutliplication is " + sum);
+    } else if (mathFunction = "divide") {
+        divide(a,b);
+        console.log("Division is " + sum);
+    } else if (mathFunction = "clearTotal") {
+        clearTotal()
+    } else {
+        console.log("Invalid Input - Try again dummy.");
+    }
+ }
 
 // Calculator container creation
 // build large rectangle container for top of calculator that displays input - 3X taller than buttons
