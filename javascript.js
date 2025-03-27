55955c0c23e4ccb616da0518f39a0e70ba7eb997
Romain Gremaud <romaingremaud@mac.attlocal.net>
2025-03-26 22:15:25 -0400

    Remove array methods and updated functions
    Removed the logic to use arrays as part of the calculations.  Added in
    click events referencing to udpate the display with the current number.

--- javascript.js
+++ javascript.js
@@ -1,10 +1,11 @@
 // initial values
 
-let sum = 0;
 let firstNumber = 0;
 let secondNumber = 0;
-let arrayA = [];
-let arrayB = [];
+let displayNumber = 0;
+let displayElement = 0;
+let buttonValue = 0;
+let storedValue = 0;
 
 // functions
 
@@ -48,56 +49,31 @@ function operate(firstNumber,secondNumber,mathFunction) {
     }
  }
 
-// button click event for numbers
-const numbers = document.querySelectorAll(".numbers");
-
-numbers.forEach((button) => {
-    button.addEventListener("click",function(event) {
-        const value = event.target.dataset.value;
-        arrayA.push(value);
-
-        console.log("Array is " + arrayA);
-    });
-});
+function concatNumbers() {
+    displayElement = document.getElementById("displayScreen");
+    displayNumber = displayElement.textContent;
+    buttonValue = this.textContent;
+  
+    if (displayNumber === "0") {
+      displayElement.textContent = buttonValue;
+    } else {
+      displayElement.textContent = displayNumber + buttonValue;
+      storedValue = Number(displayNumber + buttonValue);
+    }
+  }
+  
+  const numberButtons = document.querySelectorAll(".numbers");
+  numberButtons.forEach(button => {
+    button.addEventListener("click", concatNumbers);
+  });
 
 const clear = document.querySelector("#clear")
 clear.addEventListener("click", () => {
-    alert("ouch!");
+    displayElement.textContent = 0;
 });
 
 const deleteNumber = document.querySelector('#deleteNumber')
 deleteNumber.addEventListener("click", () => {
-    arrayA.pop();
-
-    console.log("Array is " + arrayA);
+    displayElement.textContent = displayNumber - buttonValue;
+    //displayElement.slice(0,-1);
 });
-
- // convert current number array and store as number
-  
-function arrayToNumber(array) {
-    let string = arrayA.join('');
-    let number = Number(string);
-    return number
-}
-
-
-
-
-// Enter first number 
-// 1) steps to store your number
-    // arrayA = []
-    // arrayA.push(value) adds the value to the end of the array
-// 2) select your operator (+, -, /, *)
-    // convert current number array and store as first number
-    // stringNumA = arrayA.join(''); to convert to string
-    // let numA = Number(stringNumA); to convert to a number
-
-// Calculator will always have store two variables
-// first number (or running total of sum)
-// second number being added/divided/subtracted/multiplied against first number
-
-
-//Unique buttons
-//percentage number / 100 = percentage
-// +/- = multiply by -1 to toggle
-// del -> remove the final string
